2c446fb90657e8ce90b18ad08d0fe8d053f245ea
IssouLeDingo <60429644+IssouLeDingo@users.noreply.github.com>
2020-06-12 12:52:32 +0200

    Update

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -11,10 +11,17 @@
             </li>
             <li><a href="histoire.php"><?php echo histoire?></a></li>
             <li><a href="prepvisite.php"><?php echo prepvisite?></a></li>
-            <li><a href="login.php"><?php echo connect?></a></li>
-          <li><a href="register.php"><?php echo inscrire?></a></li>
+            <li><a href="galerie.php"><?php echo galeriephoto?></a></li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn"> <?php echo compte?> </a>
+                    <div class="dropdown-content">
+                        <a href="login.php"><?php echo connect?></a>
+                        <a href="register.php"><?php echo inscrire?></a>
+                    </div>
+            </li>
+          <li><a href="quisommesnous.php"><?php echo quisommesnous?></a></li>
           <li><a href="<?php echo $_SERVER["PHP_SELF"]; ?>?lang=fr">
-          <img src="Images/français.png" height = "16px"
+          <img src="Images/francais.png" height = "16px"
           width = "25px" alt="eng" align = "center" style = "margin: 0"/></a><li/>
           <li><a href="<?php echo $_SERVER["PHP_SELF"]; ?>?lang=en">
           <img src="Images/anglais.png" height = "16px"
---
+++ quisommesnous.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+  <?php include_once('includes/header.php') ?>
+  <body>
+  <?php include_once('includes/navbar.php')?>
+        <div class="main">
+
+          <h1 id="noussommesAboutUs"><?php echo noussommes ?></h1>
+          <div class="tableAboutUs">
+
+                  <div id="firstContainerAboutUs" class="containerAboutUs">
+                    <table>
+                      <tr>
+                        <td><div class="nameAboutUs"><h2>DOMINGUES Vincent</h2></div></td>
+                      </tr>
+                      <tr>
+                        <td><img src="Images/vincent.png"></td>
+                      </tr>
+                      <tr>
+                        <td><?php echo communiquant?></td>
+                      </tr>
+                      <tr>
+                        <td><a href="https://www.linkedin.com/in/vincent-domingues-6956881a0/" target = "_blank"><img class="linkedInAboutUs" border="0" alt="Linked-in" src="Images/linkedin.png"></a></td>
+                      </tr>
+                    </table>
+                  </div>
+
+                  <div class="containerAboutUs">
+                    <table>
+                      <tr>
+                        <td><div class="nameAboutUs"><h2>FRANCISCO Michel<h2></div></td>
+                      </tr>
+                      <tr>
+                        <td><img src="Images/michel.png"></td>
+                      </tr>
+                      <tr>
+                        <td><?php echo chercheur?></td>
+                      </tr>
+                      <tr>
+                        <td><a href="https://www.linkedin.com/in/michel-francisco-5395371a1/" target = "_blank"><img class="linkedInAboutUs" border="0" alt="Linked-in" src="Images/linkedin.png"></a></td>
+                      </tr>
+                    </table>
+                  </div>
+
+                  <div class="containerAboutUs">
+                    <table>
+                      <tr>
+                        <td><div class="nameAboutUs"><h2>BOULET Quentin<h2></div></td>
+                      </tr>
+                      <tr>
+                        <td><img src="Images/quentin.png"></td>
+                      </tr>
+                      <tr>
+                        <td><?php echo chef?></td>
+                      </tr>
+                      <tr>
+                        <td><a href="https://www.linkedin.com/in/quentin-boulet-2869191a0/" target = "_blank"><img class="linkedInAboutUs" border="0" alt="Linked-in" src="Images/linkedin.png"></a></td>
+                      </tr>
+                    </table>
+                  </div>
+
+                  <div class="containerAboutUs">
+                    <table>
+                      <tr>
+                        <td><div class="nameAboutUs"><h2>GARCIA CALZADA Alan<h2></div></td>
+                      </tr>
+                      <tr>
+                        <td><img src="Images/alan.png"></td>
+                      </tr>
+                      <tr>
+                        <td><?php echo designer?></td>
+                      </tr>
+                      <tr>
+                        <td><a href="https://www.linkedin.com/in/alan-garcia-calzada-1a2b7b19b/" target = "_blank"><img class="linkedInAboutUs" border="0" alt="Linked-in" src="Images/linkedin.png"></a></td>
+                      </tr>
+                    </table>
+                  </div>
+
+                  <div class="containerAboutUs">
+                    <table>
+                      <tr>
+                        <td><div class="nameAboutUs"><h2>GUEFFIER Benjamin<h2></div></td>
+                      </tr>
+                      <tr>
+                        <td><img src="Images/benjamin.png"></td>
+                      </tr>
+                      <tr>
+                        <td><?php echo developpeur?></td>
+                      </tr>
+                      <tr>
+                        <td><a href="https://www.linkedin.com/in/benjamin-gueffier-669a0319b/" target = "_blank"><img class="linkedInAboutUs" border="0" alt="Linked-in" src="Images/linkedin.png"></a></td>
+                      </tr>
+                    </table>
+                  </div>
+
+                  <div class="containerAboutUs">
+                    <table>
+                      <tr>
+                        <td><div class="nameAboutUs"><h2>GANDEGA Issa<h2></div></td>
+                      </tr>
+                      <tr>
+                        <td><img src="Images/issa.png"></td>
+                      </tr>
+                      <tr>
+                        <td><?php echo developpeur?></td>
+                      </tr>
+                      <tr>
+                        <td><a href="https://www.linkedin.com/in/issa-gandega-5225371a1/" target = "_blank"><img class="linkedInAboutUs" border="0" alt="Linked-in" src="Images/linkedin.png"></a></td>
+                      </tr>
+                    </table>
+                  </div>
+            </div>
+        </div>
+
+<?php include_once('includes/footer.php') ?>
+</body>
+</html>
